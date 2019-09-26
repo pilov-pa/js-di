a3495b70b8b7e30fe0ee85eaf535ce0e4f971053
Aleksey Pilov <a.pilov@propellerads.net>
2019-09-26 20:04:17 +0300

    rebuild

---
+++ dist/Service.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var Service = function Service(name, className) {
+  var args = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+  var shared = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : true;
+
+  _classCallCheck(this, Service);
+
+  _defineProperty(this, "name", void 0);
+
+  _defineProperty(this, "className", void 0);
+
+  _defineProperty(this, "shared", true);
+
+  _defineProperty(this, "args", []);
+
+  _defineProperty(this, "compiled", null);
+
+  this.shared = shared;
+  this.name = name;
+  this.className = className;
+  this.args = args;
+};
+
+exports["default"] = Service;
+//# sourceMappingURL=Service.js.map
