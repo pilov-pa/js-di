ae6f6f144111e813946810c2e0e859f16c5ae67f
Aleksey Pilov <a.pilov@propellerads.net>
2019-09-30 20:10:04 +0300

    eslint tests

--- test/test.js
+++ test/test.js
@@ -1,95 +1,91 @@
 let DI = require('../dist/index').default;
 const assert = require('chai').assert;
 
-describe("js-di", function () {
-    describe("add", function () {
-        it("add class", function () {
-            class Foo {
-            }
+describe('js-di', function() {
+    describe('add', function() {
+        it('add class', function() {
+            class Foo {}
             const di = new DI();
-            di.add("foo", Foo);
-            const resolved = di.resolve("foo");
+            di.add('foo', Foo);
+            const resolved = di.resolve('foo');
             assert.equal(Foo.prototype.isPrototypeOf(resolved), true);
         });
 
-        it("add already exists", function () {
-            class Foo {
-            }
+        it('add already exists', function() {
+            class Foo {}
             const di = new DI();
-            di.add("foo", Foo);
+            di.add('foo', Foo);
             function add() {
-                di.add("foo", Foo);
+                di.add('foo', Foo);
             }
             assert.throws(add, "Service 'foo' already exists");
         });
 
-        it("add not shared", function() {
+        it('add not shared', function() {
             let v = 0;
             class Foo {
-                constructor()
-                {
+                constructor() {
                     v++;
                     this.c = v;
                 }
             }
             const di = new DI();
-            di.add("foo", Foo, [], false);
-            const resolved1 = di.resolve("foo");
-            const resolved2 = di.resolve("foo");
+            di.add('foo', Foo, [], false);
+            const resolved1 = di.resolve('foo');
+            const resolved2 = di.resolve('foo');
 
             assert.notEqual(resolved1.c, resolved2.c);
         });
-        it("add shared", function() {
+        it('add shared', function() {
             let v = 0;
             class Foo {
                 static v = 0;
-                constructor()
-                {
+                constructor() {
                     v++;
                     this.c = v;
                 }
             }
             const di = new DI();
-            di.add("foo", Foo, []);
-            const resolved1 = di.resolve("foo");
-            const resolved2 = di.resolve("foo");
+            di.add('foo', Foo, []);
+            const resolved1 = di.resolve('foo');
+            const resolved2 = di.resolve('foo');
 
             assert.equal(resolved1.c, resolved2.c);
         });
     });
 
-    describe("addParameters", function() {
-        it("addParameters", function () {
+    describe('addParameters', function() {
+        it('addParameters', function() {
             const di = new DI();
             di.addParameters({
                 foo: 'String',
-                bar: 'Second string'
+                bar: 'Second string',
             });
 
-            assert.equal(di.getParameter("foo"), "String");
-            assert.equal(di.getParameter("bar"), "Second string");
+            assert.equal(di.getParameter('foo'), 'String');
+            assert.equal(di.getParameter('bar'), 'Second string');
         });
-        it("addParameters already exists", function () {
+        it('addParameters already exists', function() {
             const di = new DI();
             di.addParameters({
                 foo: 'String',
-                bar: 'Second string'
+                bar: 'Second string',
             });
             function addParameters() {
                 di.addParameters({
-                    bar: 'Second string'
+                    bar: 'Second string',
                 });
             }
             assert.throws(addParameters, "Parameter 'bar' already exists");
         });
     });
 
-    describe("removeParameter", function() {
-        it("removeParameter success", function() {
+    describe('removeParameter', function() {
+        it('removeParameter success', function() {
             const di = new DI();
             di.addParameters({
                 foo: 'String',
-                bar: 'Second string'
+                bar: 'Second string',
             });
 
             di.removeParameter('foo');
@@ -99,7 +95,7 @@ describe("js-di", function () {
             assert.throws(getParameter, "Parameter 'foo' not found");
         });
 
-        it("removeParameter not found", function() {
+        it('removeParameter not found', function() {
             const di = new DI();
 
             function removeParameter() {
@@ -109,57 +105,56 @@ describe("js-di", function () {
         });
     });
 
-    describe("resolve", function() {
-        it("resolve service not found", function() {
+    describe('resolve', function() {
+        it('resolve service not found', function() {
             const di = new DI();
 
             function resolve() {
-                const resolved = di.resolve("foo");
+                const resolved = di.resolve('foo');
             }
             assert.throws(resolve, "Service 'foo' not found");
-        })
+        });
     });
 
-    describe("remove", function() {
-        it("remove", function() {
-            class Foo {
-            }
+    describe('remove', function() {
+        it('remove', function() {
+            class Foo {}
             const di = new DI();
-            di.add("foo", Foo);
-            di.remove("foo");
+            di.add('foo', Foo);
+            di.remove('foo');
             function resolve() {
-                const resolved = di.resolve("foo");
+                const resolved = di.resolve('foo');
             }
             assert.throws(resolve, "Service 'foo' not found");
         });
 
-        it("remove not found", function() {
+        it('remove not found', function() {
             const di = new DI();
             function remove() {
-                di.remove("foo");
+                di.remove('foo');
             }
             assert.throws(remove, "Service 'foo' not found");
         });
     });
 
-    describe("has", function() {
-        it("has true", function() {
+    describe('has', function() {
+        it('has true', function() {
             class Foo {}
             const di = new DI();
-            di.add("foo", Foo);
+            di.add('foo', Foo);
 
             assert.equal(di.has('foo'), true);
         });
 
-        it("has false", function() {
+        it('has false', function() {
             const di = new DI();
 
             assert.equal(di.has('foo'), false);
         });
     });
 
-    describe("integration", function() {
-        it("js-di", function () {
+    describe('integration', function() {
+        it('js-di', function() {
             class Foo {
                 constructor(bar) {
                     this.bar = bar;
@@ -177,12 +172,12 @@ describe("js-di", function () {
             }
 
             let di = new DI();
-            di.add("foo", Foo, ["bar"]);
-            di.add("bar", Bar, ["@bar_name"]);
+            di.add('foo', Foo, ['bar']);
+            di.add('bar', Bar, ['@bar_name']);
             di.addParameters({
-                'bar_name': 'Bar name!',
+                bar_name: 'Bar name!',
             });
-            const foo = di.resolve("foo");
+            const foo = di.resolve('foo');
             assert.equal(foo.getBarName(), 'Bar name!');
         })
     });
